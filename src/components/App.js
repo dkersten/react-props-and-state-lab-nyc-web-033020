ea0a15be08e5ba330875ded012fa4ced49ec4994
dkersten <dkersten1994@gmail.com>
2020-06-04 09:15:43 -0400

    finished first 4 deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
+import allPets from '../data/pets.js'
 
 class App extends React.Component {
   constructor() {
@@ -15,7 +16,34 @@ class App extends React.Component {
     }
   }
 
+  selectValue = (value) => {
+    this.setState({
+      filters: {
+        type: value
+      }
+    })
+  }
+
+  petType = () => {
+    // console.log("test")
+
+    if (this.state.filters.type === "all") {
+      fetch(`/api/pets`)
+      .then(resp => resp.json())
+      .then(data => this.setState({
+        pets: data
+      }))
+    } else {
+      fetch(`/api/pets?type=${this.state.filters.type}`)
+      .then(resp => resp.json())
+      .then(data => this.setState({
+        pets: data
+      }))
+    }
+  }
+
   render() {
+    // console.log(this.state.filters.type)
     return (
       <div className="ui container">
         <header>
@@ -24,10 +52,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onFindPetsClick={this.petType} onChangeType={this.selectValue} typeValue={this.state.filters.type} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} />
             </div>
           </div>
         </div>
